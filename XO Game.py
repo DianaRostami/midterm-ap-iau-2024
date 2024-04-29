eb2f489d5b2cd188a4d2fe64ecc05a8cf876687a
panamasoumi <163293671+panamasoumi@users.noreply.github.com>
2024-04-29 20:34:45 +0330

    second commit

--- XO Game.py
+++ XO Game.py
@@ -1,4 +1,8 @@
-def print_board(board):
-    for row in board:
-        print(" | ".join(row))
-        print("-" * 5)
+class Player:
+    def __init__(self, name, symbol):
+        self.name = name
+        self.symbol = symbol
+class GameBoard:
+    def __init__(self):
+        self.board = [[' ' for _ in range(3)] for _ in range(3)]
+
