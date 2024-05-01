7e866fba39264a732a0d44ab80bbb96ce2050cef
DianaRostami <diana84rostami@gmail.com>
2024-05-01 13:57:27 +0330

    initial commit

---
+++ password mindmap.mm
@@ -0,0 +1,41 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1713889812769" ID="ID_1652834526" MODIFIED="1713889938748" TEXT=" password generation program">
+<font NAME="SansSerif" SIZE="28"/>
+<node CREATED="1713889942221" HGAP="94" ID="ID_848518240" MODIFIED="1713890083119" POSITION="right" TEXT="numbers of password" VSHIFT="-199">
+<font NAME="SansSerif" SIZE="24"/>
+<node CREATED="1713889995992" HGAP="25" ID="ID_1153106184" MODIFIED="1713890582136" STYLE="bubble" TEXT="more than12 character" VSHIFT="-77">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node CREATED="1713890056125" HGAP="60" ID="ID_926058746" MODIFIED="1713890577313" STYLE="bubble" TEXT="less than 19 character" VSHIFT="51">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1713890086281" HGAP="95" ID="ID_434200327" MODIFIED="1713890158945" POSITION="left" TEXT="+ conditions" VSHIFT="-188">
+<font NAME="SansSerif" SIZE="24"/>
+<node CREATED="1713890163128" HGAP="24" ID="ID_593464639" MODIFIED="1713890613524" STYLE="bubble" TEXT="at least &quot;1&quot; small english letter" VSHIFT="-86">
+<arrowlink DESTINATION="ID_593464639" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1211315938" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_593464639" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1211315938" SOURCE="ID_593464639" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node CREATED="1713890244104" HGAP="72" ID="ID_956553445" MODIFIED="1713890626165" STYLE="bubble" TEXT="at least &quot;1&quot; capital english letter" VSHIFT="-6">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node CREATED="1713890308571" HGAP="75" ID="ID_1108223368" MODIFIED="1713890595908" STYLE="bubble" TEXT="one character (#,@) " VSHIFT="88">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1713890221945" HGAP="108" ID="ID_890152411" MODIFIED="1713890238013" POSITION="left" TEXT="- conditions" VSHIFT="129">
+<font NAME="SansSerif" SIZE="24"/>
+<node CREATED="1713890355363" HGAP="88" ID="ID_318023176" MODIFIED="1713890590529" STYLE="bubble" TEXT="don&apos;t use any english vowels" VSHIFT="-16">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1713890461631" HGAP="94" ID="ID_1222666422" MODIFIED="1713890518023" POSITION="right" TEXT="generate 10 sample " VSHIFT="137">
+<font NAME="SansSerif" SIZE="24"/>
+<node CREATED="1713890524659" HGAP="112" ID="ID_1820892676" MODIFIED="1713890570109" STYLE="bubble" TEXT="text file" VSHIFT="-34">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</node>
+</map>
---
+++ text mindmap.mm
@@ -0,0 +1,69 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1714055240658" ID="ID_1895765234" MODIFIED="1714055316108" TEXT="text file reader program">
+<font NAME="SansSerif" SIZE="24"/>
+<node CREATED="1714055323502" HGAP="84" ID="ID_319312727" MODIFIED="1714056055404" POSITION="right" STYLE="bubble" TEXT="name " VSHIFT="-67">
+<font NAME="SansSerif" SIZE="24"/>
+<node CREATED="1714055345466" ID="ID_1163359858" MODIFIED="1714055387825" TEXT="input.txt">
+<font NAME="SansSerif" SIZE="20"/>
+</node>
+</node>
+<node CREATED="1714055395998" HGAP="43" ID="ID_1293640704" MODIFIED="1714055798421" POSITION="left" STYLE="bubble" TEXT="number of consonats" VSHIFT="-77">
+<font NAME="SansSerif" SIZE="24"/>
+</node>
+<node CREATED="1714055513352" HGAP="66" ID="ID_763107835" MODIFIED="1714056052889" POSITION="right" STYLE="bubble" TEXT="number of every english vowels" VSHIFT="-71">
+<font NAME="SansSerif" SIZE="24"/>
+<node CREATED="1714055568566" ID="ID_490282566" MODIFIED="1714055606087" TEXT="like&quot; a,b,c&quot;">
+<font NAME="SansSerif" SIZE="20"/>
+</node>
+</node>
+<node CREATED="1714055719441" HGAP="65" ID="ID_72137421" MODIFIED="1714055930072" POSITION="left" STYLE="bubble" TEXT="nubmer of sentences" VSHIFT="-29">
+<font NAME="SansSerif" SIZE="24"/>
+</node>
+<node CREATED="1714055935063" HGAP="99" ID="ID_914604988" MODIFIED="1714056059168" POSITION="right" STYLE="bubble" TEXT="number of words" VSHIFT="14">
+<font NAME="SansSerif" SIZE="24"/>
+<node CREATED="1714056004188" ID="ID_990124871" MODIFIED="1714056111212" TEXT="number of their repetitions">
+<font NAME="SansSerif" SIZE="20"/>
+</node>
+</node>
+<node CREATED="1714056068757" HGAP="45" ID="ID_1019930469" MODIFIED="1714056183546" POSITION="left" STYLE="bubble" TEXT="  number of repetitions" VSHIFT="31">
+<font NAME="SansSerif" SIZE="24"/>
+<node CREATED="1714056168219" HGAP="45" ID="ID_1159620542" MODIFIED="1714056208355" TEXT="every letter" VSHIFT="-11">
+<font NAME="SansSerif" SIZE="20"/>
+</node>
+<node CREATED="1714056187180" HGAP="19" ID="ID_1260217656" MODIFIED="1714056201340" TEXT="every number" VSHIFT="21">
+<font NAME="SansSerif" SIZE="20"/>
+</node>
+</node>
+<node CREATED="1714056212997" HGAP="89" ID="ID_578669849" MODIFIED="1714056255395" POSITION="right" STYLE="bubble" TEXT="number of pharagraps" VSHIFT="61">
+<font NAME="SansSerif" SIZE="24"/>
+</node>
+<node CREATED="1714056261196" HGAP="62" ID="ID_274977352" MODIFIED="1714056334645" POSITION="left" STYLE="bubble" TEXT="txt file as output" VSHIFT="43">
+<font NAME="SansSerif" SIZE="24"/>
+</node>
+<node CREATED="1714056340052" HGAP="75" ID="ID_198547099" MODIFIED="1714056383255" POSITION="right" STYLE="bubble" TEXT="excel output" VSHIFT="57">
+<font NAME="SansSerif" SIZE="24"/>
+<node CREATED="1714056785744" ID="ID_184829743" MODIFIED="1714056815604" TEXT="using Pandas library">
+<font NAME="SansSerif" SIZE="20"/>
+</node>
+</node>
+<node CREATED="1714056392486" HGAP="37" ID="ID_1933843119" MODIFIED="1714056558199" POSITION="left" STYLE="bubble" TEXT="variance and average" VSHIFT="61">
+<font NAME="SansSerif" SIZE="24"/>
+<node CREATED="1714056487628" HGAP="23" ID="ID_707486244" MODIFIED="1714056714805" TEXT="of consonats " VSHIFT="-25">
+<font NAME="SansSerif" SIZE="20"/>
+<node CREATED="1714056719514" ID="ID_1312630042" MODIFIED="1714056747938" TEXT="in each sentence , paraghrap and text">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node CREATED="1714056563467" HGAP="23" ID="ID_1968513387" MODIFIED="1714056712035" TEXT="of of vowels " VSHIFT="8">
+<font NAME="SansSerif" SIZE="20"/>
+<node CREATED="1714056678768" ID="ID_1650418182" MODIFIED="1714056755949" TEXT="in each sentence , paraghraps and text">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+<node CREATED="1714056827489" HGAP="-3" ID="ID_737185392" MODIFIED="1714056867413" POSITION="right" TEXT="using nltk snd spacy library in python" VSHIFT="109">
+<font NAME="SansSerif" SIZE="24"/>
+</node>
+</node>
+</map>
